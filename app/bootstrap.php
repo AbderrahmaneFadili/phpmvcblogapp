a3b2c7e5307f2194919cb6c8e2815e4a558bd903
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-10-28 16:58:47 +0100

    create app folder structure

---
+++ app/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Bootstrap is the first piece of code your web application runs. It loads all 
+ * system components, and then runs your application. The component uses MVC 
+ * application architecture.
+ */
